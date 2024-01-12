f09f30a864fa6c5fd0e4927d7fd47848a6ec1cef
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-01-12 05:43:04 +0300

    Changed code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,17 +6,17 @@ def canUnlockAll(boxes):
     """
     Determines whether the boxes can be locked or not.
     """
-    total_boxes = len(boxes)
-    setofkeys = [0]
+    all_boxes = len(boxes)
+    keyset = [0]
     counter = 0
     index = 0
 
-    while index < len(setofkeys):
-        setkey = setofkeys[index]
+    while index < len(keyset):
+        setkey = keyset[index]
         for key in boxes[setkey]:
-            if 0 < key < total_boxes and key not in setofkeys:
-                setofkeys.append(key)
+            if 0 < key < all_boxes and key not in keyset:
+                keyset.append(key)
                 counter += 1
         index += 1
 
-    return counter == total_boxes - 1
+    return counter == all_boxes - 1
