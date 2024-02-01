d155aed93dc42a1f128a3bf35a6d1fe10428cd2a
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-02-01 09:00:42 +0300

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,34 +16,34 @@ def validUTF8(data):
     Method that determines if a given data set represents a valid
     UTF-8 encoding.
     """
-    number_bytes = 0
+    remaining_bytes = 0
 
-    mask_1 = 1 << 7
-    mask_2 = 1 << 6
+    mask_UTF8_BIT_1 = 1 << 7 # 10000000
+    mask_UTF8_BIT_2 = 1 << 6 # 01000000
 
     for i in data:
 
         mask_byte = 1 << 7
 
-        if number_bytes == 0:
+        if remaining_bytes == 0:
 
             while mask_byte & i:
-                number_bytes += 1
+                remaining_bytes += 1
                 mask_byte = mask_byte >> 1
 
-            if number_bytes == 0:
+            if remaining_bytes == 0:
                 continue
 
-            if number_bytes == 1 or number_bytes > 4:
+            if remaining_bytes == 1 or remaining_bytes > 4:
                 return False
 
         else:
-            if not (i & mask_1 and not (i & mask_2)):
+            if not (i & mask_UTF8_BIT_1 and not (i & mask_UTF8_BIT_2)):
                     return False
 
-        number_bytes -= 1
+        remaining_bytes -= 1
 
-    if number_bytes == 0:
+    if remaining_bytes == 0:
         return True
 
     return False
