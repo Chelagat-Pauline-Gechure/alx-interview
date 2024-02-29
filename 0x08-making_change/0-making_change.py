4159021d43806e0bf6f797f263da4d2d8bb78626
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-02-29 12:31:08 +0000

    0. Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Change Making Problem """
+
+
+def makeChange(coins, total):
+    """ Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total.
+    Prototype: def makeChange(coins, total)
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    coins is a list of the values of the coins in your possession
+    The value of a coin will always be an integer greater than 0
+    You can assume you have an infinite number of each denomination of coin in the list
+    Your solution’s runtime will be evaluated in this task
+    Args:
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
+    """
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
